29a93867c8da691c00c8c0f465c99cdf9180ed25
VieiraHub <vieiraonwork@gmail.com>
2020-12-27 23:17:44 +0000

    Insert H2 database and create automation of DB in Category class.

--- demostore/src/main/java/com/vieiraatelier/demostore/domain/Category.java
+++ demostore/src/main/java/com/vieiraatelier/demostore/domain/Category.java
@@ -2,10 +2,17 @@ package com.vieiraatelier.demostore.domain;
 
 import java.io.Serializable;
 
-//@Ent
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Category implements Serializable {
 	private static final long serialVersionUID = -9013424695179346807L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String name;
 	
