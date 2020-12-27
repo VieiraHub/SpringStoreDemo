6a06d606c3412a990218c1ac153e9780af5448aa
VieiraHub <vieiraonwork@gmail.com>
2020-12-27 19:46:26 +0000

    First commit.

---
+++ demostore/src/main/java/com/vieiraatelier/demostore/DemostoreApplication.java
@@ -0,0 +1,13 @@
+package com.vieiraatelier.demostore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemostoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemostoreApplication.class, args);
+	}
+
+}
