6fdf4955b74a61cd501b21fe177542ee964bad8b
VieiraHub <vieiraonwork@gmail.com>
2020-12-27 22:49:57 +0000

    Create and test first domain class Category.

--- demostore/src/main/java/com/vieiraatelier/demostore/resources/CategoryResource.java
+++ demostore/src/main/java/com/vieiraatelier/demostore/resources/CategoryResource.java
@@ -1,15 +1,27 @@
 package com.vieiraatelier.demostore.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.vieiraatelier.demostore.domain.Category;
 
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResource {
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String testing() {
-		return "REST is working!";
+	public List<Category> listing() {
+		Category cat1 = new Category(1, "Computing");
+		Category cat2 = new Category(2, "Office");
+		
+		List<Category> list = new ArrayList<>();
+		list.add(cat1);
+		list.add(cat2);
+		
+		return list;
 	}
 }
