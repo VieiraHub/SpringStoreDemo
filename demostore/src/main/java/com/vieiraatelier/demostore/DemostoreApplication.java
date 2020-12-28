afe484426e60c2d691f76173d99b1d0b7d722506
VieiraHub <vieiraonwork@gmail.com>
2020-12-28 17:51:36 +0000

    Create State and City classes.

--- demostore/src/main/java/com/vieiraatelier/demostore/DemostoreApplication.java
+++ demostore/src/main/java/com/vieiraatelier/demostore/DemostoreApplication.java
@@ -8,9 +8,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.vieiraatelier.demostore.domain.Category;
+import com.vieiraatelier.demostore.domain.City;
 import com.vieiraatelier.demostore.domain.Product;
+import com.vieiraatelier.demostore.domain.State;
 import com.vieiraatelier.demostore.repositories.CategoryRepository;
+import com.vieiraatelier.demostore.repositories.CityRepository;
 import com.vieiraatelier.demostore.repositories.ProductRepository;
+import com.vieiraatelier.demostore.repositories.StateRepository;
 
 @SpringBootApplication
 public class DemostoreApplication implements CommandLineRunner {
@@ -20,6 +24,12 @@ public class DemostoreApplication implements CommandLineRunner {
 	
 	@Autowired
 	private ProductRepository productRepository; 
+	
+	@Autowired
+	private StateRepository stateRepository;
+	
+	@Autowired
+	private CityRepository cityRepository; 
 	
 	public static void main(String[] args) {
 		SpringApplication.run(DemostoreApplication.class, args);
@@ -43,6 +53,19 @@ public class DemostoreApplication implements CommandLineRunner {
 		
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2));
 		productRepository.saveAll(Arrays.asList(p1, p2, p3));
+		
+		State s1 = new State(null, "California");
+		State s2 = new State(null, "Texas");
+		
+		City c1 = new City(null, "Los Angeles", s1);
+		City c2 = new City(null, "Houston", s2);
+		City c3 = new City(null, "Dallas", s2);
+		
+		s1.getCities().addAll(Arrays.asList(c1));
+		s2.getCities().addAll(Arrays.asList(c2, c3));
+		
+		stateRepository.saveAll(Arrays.asList(s1, s2));
+		cityRepository.saveAll(Arrays.asList(c1, c2, c3));
 	}
 
 }
---
+++ demostore/src/main/java/com/vieiraatelier/demostore/repositories/CityRepository.java
@@ -0,0 +1,11 @@
+package com.vieiraatelier.demostore.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.vieiraatelier.demostore.domain.City;
+
+@Repository
+public interface CityRepository extends JpaRepository<City, Integer> {
+
+}
