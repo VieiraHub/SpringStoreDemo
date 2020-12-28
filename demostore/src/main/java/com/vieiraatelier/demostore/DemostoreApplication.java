a6ff0ab52371fe1505e4cbf26a1c504ca4364ed7
VieiraHub <vieiraonwork@gmail.com>
2020-12-28 00:18:33 +0000

    Create instantiation of Category elements in DB.

--- demostore/src/main/java/com/vieiraatelier/demostore/DemostoreApplication.java
+++ demostore/src/main/java/com/vieiraatelier/demostore/DemostoreApplication.java
@@ -1,13 +1,31 @@
 package com.vieiraatelier.demostore;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class DemostoreApplication {
+import com.vieiraatelier.demostore.domain.Category;
+import com.vieiraatelier.demostore.repositories.CategoryRepository;
 
+@SpringBootApplication
+public class DemostoreApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoryRepository categoryRepository; 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DemostoreApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Category cat1 = new Category(null, "Computing");
+		Category cat2 = new Category(null, "Office");
+		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
