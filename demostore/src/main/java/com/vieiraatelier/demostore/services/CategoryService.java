c074a9fca80ab7c083fdd18729870fe59fa66ffa
VieiraHub <vieiraonwork@gmail.com>
2020-12-28 15:29:54 +0000

    Endpoint /categories/{id} adjustments.

--- demostore/src/main/java/com/vieiraatelier/demostore/services/CategoryService.java
+++ demostore/src/main/java/com/vieiraatelier/demostore/services/CategoryService.java
@@ -7,15 +7,17 @@ import org.springframework.stereotype.Service;
 
 import com.vieiraatelier.demostore.domain.Category;
 import com.vieiraatelier.demostore.repositories.CategoryRepository;
+import com.vieiraatelier.demostore.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoryService {
 
 	@Autowired
 	private CategoryRepository repo;
-	
+
 	public Category find(Integer id) {
 		Optional<Category> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Object not found! Id: " + id + ", Type: " + Category.class.getName()));
 	}
 }
---
+++ demostore/src/main/java/com/vieiraatelier/demostore/services/exceptions/ObjectNotFoundException.java
@@ -0,0 +1,13 @@
+package com.vieiraatelier.demostore.services.exceptions;
+
+public class ObjectNotFoundException extends RuntimeException {
+	private static final long serialVersionUID = 7450793061443203175L;
+
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+	
+	public ObjectNotFoundException(String msg, Throwable cause) {
+		super(msg, cause);
+	}
+}
