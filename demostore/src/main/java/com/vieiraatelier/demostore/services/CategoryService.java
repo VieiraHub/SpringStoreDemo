58e23bb6f9c305577ddf99866af3fc2b60e48156
VieiraHub <vieiraonwork@gmail.com>
2020-12-30 21:57:22 +0000

    Corrections: update on CategoryService and Payment builder.

--- demostore/src/main/java/com/vieiraatelier/demostore/services/CategoryService.java
+++ demostore/src/main/java/com/vieiraatelier/demostore/services/CategoryService.java
@@ -34,8 +34,9 @@ public class CategoryService {
 	}
 	
 	public Category update(Category obj) {
-		find(obj.getId());
-		return repo.save(obj);
+		Category newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
 	}
 	
 	public void delete(Integer id) {
@@ -58,5 +59,9 @@ public class CategoryService {
 	
 	public Category fromDTO(CategoryDTO objDto) {
 		return new Category(objDto.getId(), objDto.getName());
+	}
+	
+	private void updateData(Category newObj, Category obj) {
+		newObj.setName(obj.getName());
 	}
 }
