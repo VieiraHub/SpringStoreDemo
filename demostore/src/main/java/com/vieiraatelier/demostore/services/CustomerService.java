85b7ad05353471e05310e9422fe68f45db094195
VieiraHub <vieiraonwork@gmail.com>
2020-12-30 22:51:49 +0000

    Insert a new Customer with POST.

--- demostore/src/main/java/com/vieiraatelier/demostore/services/CustomerService.java
+++ demostore/src/main/java/com/vieiraatelier/demostore/services/CustomerService.java
@@ -9,9 +9,15 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.vieiraatelier.demostore.domain.Address;
+import com.vieiraatelier.demostore.domain.City;
 import com.vieiraatelier.demostore.domain.Customer;
+import com.vieiraatelier.demostore.domain.enums.CustomerType;
 import com.vieiraatelier.demostore.dto.CustomerDTO;
+import com.vieiraatelier.demostore.dto.CustomerNewDTO;
+import com.vieiraatelier.demostore.repositories.AddressRepository;
 import com.vieiraatelier.demostore.repositories.CustomerRepository;
 import com.vieiraatelier.demostore.services.exceptions.DataIntegrityException;
 import com.vieiraatelier.demostore.services.exceptions.ObjectNotFoundException;
@@ -21,11 +27,22 @@ public class CustomerService {
 
 	@Autowired
 	private CustomerRepository repo;
+	
+	@Autowired
+	private AddressRepository addressRepo;
 
 	public Customer find(Integer id) {
 		Optional<Customer> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Object not found! Id: " + id + ", Type: " + Customer.class.getName()));
+	}
+	
+	@Transactional
+	public Customer insert(Customer obj) {
+		obj.setId(null);
+		obj = repo.save(obj);
+		addressRepo.saveAll(obj.getAddresses());
+		return obj;
 	}
 	
 	public Customer update(Customer obj) {
@@ -54,6 +71,18 @@ public class CustomerService {
 	
 	public Customer fromDTO(CustomerDTO objDto) {
 		return new Customer(objDto.getId(), objDto.getName(), objDto.getEmail(), null, null);
+	}
+	
+	public Customer fromDTO(CustomerNewDTO objDto) {
+		Customer customer = new Customer(null, objDto.getName(), objDto.getEmail(),objDto.getTaxPayerNumber(), CustomerType.toEnum(objDto.getType()));
+		City city = new City(objDto.getCityId(), null, null);
+		Address address = new Address(null, objDto.getStreet(), objDto.getNumber(), objDto.getComplement(), objDto.getNeighborhood(), objDto.getPostalCode(), city, customer);
+		customer.getAddresses().add(address);
+		customer.getPhones().add(objDto.getPhone1());
+		if (objDto.getPhone2() != null) customer.getPhones().add(objDto.getPhone2());
+		if (objDto.getPhone3() != null) customer.getPhones().add(objDto.getPhone3());
+		
+		return customer;
 	}
 	
 	private void updateData(Customer newObj, Customer obj) {
