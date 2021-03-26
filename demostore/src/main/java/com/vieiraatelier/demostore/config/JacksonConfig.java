466dc71b56c5202eb593b3e17186a3e1c3232a47
VieiraHub <vieiraonwork@gmail.com>
2021-03-26 22:34:01 +0000

    Initial Spring Security setup.

--- demostore/src/main/java/com/vieiraatelier/demostore/config/JacksonConfig.java
+++ demostore/src/main/java/com/vieiraatelier/demostore/config/JacksonConfig.java
@@ -3,6 +3,8 @@ package com.vieiraatelier.demostore.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.vieiraatelier.demostore.domain.PaymentWithCard;
@@ -21,5 +23,10 @@ public class JacksonConfig {
 			};
 		};
 		return builder;
+	}
+	
+	@Bean
+	public JavaMailSender javaMailSender() {
+		return new JavaMailSenderImpl();
 	}
 }
