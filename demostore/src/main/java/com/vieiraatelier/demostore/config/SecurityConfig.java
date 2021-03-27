1fc8bfb8b2ce7f1e6dc291a8f852524e0807375d
VieiraHub <vieiraonwork@gmail.com>
2021-03-27 01:08:05 +0000

    Implement Authentication and generate JWT token.

--- demostore/src/main/java/com/vieiraatelier/demostore/config/SecurityConfig.java
+++ demostore/src/main/java/com/vieiraatelier/demostore/config/SecurityConfig.java
@@ -7,14 +7,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import com.vieiraatelier.demostore.security.JwtAuthenticationFilter;
+import com.vieiraatelier.demostore.security.JwtUtil;
 
 
 @Configuration
@@ -23,6 +28,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	private Environment env;
+	
+	@Autowired
+	private UserDetailsService userDetailsService;
+	
+	@Autowired
+	private JwtUtil jwtUtil;
 	
 	private static final String[] PUBLIC_MATCHERS = {
 			"/h2-console/**"
@@ -46,10 +57,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
 			.antMatchers(PUBLIC_MATCHERS).permitAll()
 			.anyRequest().authenticated();
+		http.addFilter(new JwtAuthenticationFilter(authenticationManager(), jwtUtil));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 		
 	}
 	
+	
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
+	}
+
+
+
 	@Bean
 	CorsConfigurationSource corsConfigurationSource() {
 		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
---
+++ demostore/src/main/java/com/vieiraatelier/demostore/services/UserDetailsServiceImpl.java
@@ -0,0 +1,30 @@
+package com.vieiraatelier.demostore.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.vieiraatelier.demostore.domain.Customer;
+import com.vieiraatelier.demostore.repositories.CustomerRepository;
+import com.vieiraatelier.demostore.security.UserSpringSecurity;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+	@Autowired
+	private CustomerRepository repo;
+
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+		Customer customer = repo.findByEmail(email);
+		if (customer == null)
+			throw new UsernameNotFoundException(email);
+
+		return new UserSpringSecurity(customer.getId(), customer.getEmail(), customer.getPassword(),
+				customer.getProfiles());
+	}
+
+}
