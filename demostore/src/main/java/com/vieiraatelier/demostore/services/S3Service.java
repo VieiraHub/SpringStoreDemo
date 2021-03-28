d6e2735c41db05157af812434cb01860c535a2ab
VieiraHub <vieiraonwork@gmail.com>
2021-03-28 23:37:08 +0100

    Handling exceptions properly.

--- demostore/src/main/java/com/vieiraatelier/demostore/services/S3Service.java
+++ demostore/src/main/java/com/vieiraatelier/demostore/services/S3Service.java
@@ -14,6 +14,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.vieiraatelier.demostore.services.exceptions.FileException;
 
 @Service
 public class S3Service {
@@ -33,7 +34,7 @@ public class S3Service {
 			String contentType = multiPartFile.getContentType();
 			return uploadFile(inputStream, fileName, contentType);
 		} catch (IOException e) {
-			throw new RuntimeException("Error of IO: " + e.getMessage());
+			throw new FileException("Error of IO: " + e.getMessage());
 		}
 
 	}
@@ -47,7 +48,7 @@ public class S3Service {
 			LOG.info("Upload successfully ended");
 			return s3Customer.getUrl(bucketName, fileName).toURI();
 		} catch (URISyntaxException e) {
-			throw new RuntimeException("Error when converting URL to URI");
+			throw new FileException("Error when converting URL to URI");
 		}
 	}
 }
